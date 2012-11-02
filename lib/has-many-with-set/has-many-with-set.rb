2619e04924d01a75a83d72e62399ea6f4002fc0e
Francisco Soto <ebobby@ebobby.org>
2012-11-01 21:56:21 -0700

    Simplified the call to has_many_with_set.
    It originally required both the parent and the child but was redundant because the parent
    name is already available at that point in self. Also simplified the relationship building
    code a little bit.

--- lib/has-many-with-set/has-many-with-set.rb
+++ lib/has-many-with-set/has-many-with-set.rb
@@ -1,16 +1,17 @@
 module HasManyWithSet
   # Add the has_many_with_set relationship type to ActiveRecord.
-  def has_many_with_set (parent, child)
-    build_set_relationship parent, child
+  def has_many_with_set (child)
+    # Article, Tags
+    build_set_relationship self.to_s, child.to_s.classify
   end
 
   private
 
-  def build_set_relationship (parent, child)
+  def build_set_relationship (parent_model_name, child_model_name)
     extend HasManyWithSet::Relationships
 
-    create_set_model(child)
-    relate_child_to_set(child)
-    relate_parent_to_set(parent, child)
+    create_set_model child_model_name
+    relate_child_to_set child_model_name
+    relate_parent_to_set parent_model_name, child_model_name
   end
 end
--- lib/has-many-with-set/relationships.rb
+++ lib/has-many-with-set/relationships.rb
@@ -1,14 +1,13 @@
 module HasManyWithSet
   module Relationships
-    def create_set_model (child)
-      set_model_name = generate_set_model_name(symbol_to_class_name(child))
+    def create_set_model (child_model_name)
+      set_model_name = generate_set_model_name(child_model_name)
 
       Object.const_set(set_model_name, Class.new(ActiveRecord::Base)) unless
         Object.const_defined?(set_model_name) # this *should* never happen...
     end
 
-    def relate_child_to_set (child)
-      child_model_name = symbol_to_class_name(child)
+    def relate_child_to_set (child_model_name)
       set_model_name = generate_set_model_name(child_model_name)
 
       # Take the child model and add a regular many-to-many relationship to the Set model...
@@ -22,10 +21,7 @@ module HasManyWithSet
       end
     end
 
-    def relate_parent_to_set (parent, child)
-      parent_model_name = symbol_to_class_name(parent)
-      child_model_name = symbol_to_class_name(child)
-
+    def relate_parent_to_set (parent_model_name, child_model_name)
       # The parent object has a FK to the Set table, so it belongs_to it.
       Object.const_get(parent_model_name).class_eval do
         belongs_to generate_set_model_name(child_model_name).tableize.singularize.to_sym
@@ -33,10 +29,6 @@ module HasManyWithSet
     end
 
     private
-
-    def symbol_to_class_name (symbol)
-      symbol.to_s.classify
-    end
 
     # Generates a name for Set models based on the child table name.
     def generate_set_model_name (target_model)
